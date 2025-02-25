b1de63bd4ac2bf3e6ab54c5fcc413855f1e0b7ba
Skeating777 <keatinsp@oregonstate.edu>
2025-02-24 20:51:08 -0800

    Initial commit

---
+++ Export.py
@@ -0,0 +1,25 @@
+import pandas
+import pathlib
+import os
+
+
+while True:
+    text = open("export_service.txt", "r")
+    content = text.read()
+    if content == "run":
+        directory = pathlib.Path.home() / "Downloads"  # Change this value to export elsewhere
+        csv_file_path = str(directory) + "/locationlist.csv"
+        text = pandas.read_csv("data.txt", delimiter="/")
+        if os.path.exists(csv_file_path):
+            i = 1
+            csv_file_path = str(directory) + "/locationlist(" + str(i) + ").csv"
+            while os.path.exists(str(directory) + "/locationlist(" + str(i) + ").csv"):
+                i += 1
+                csv_file_path = str(directory) + "/locationlist(" + str(i) + ").csv"
+        text.to_csv(csv_file_path, index=None)
+        text = open("export_response.txt", "w")
+        text.write(".csv exported to " + str(directory))
+        text.close()
+
+        text = open("export_service.txt", "w")
+        text.close()
---
+++ demo.py
@@ -0,0 +1,40 @@
+import time
+class Place:
+    def __init__(self, location, country, distance):
+        self.location = location
+        self.country = country
+        self.distance = distance
+
+
+array = [
+    Place("Cork", "IE", 4581.05),
+    Place("Mykonos", "GR", 6278.12),
+    Place("Palma", "ES", 5579.51),
+    Place("Sardinia", "IT",5731.21),
+    Place("Osaka", "JP", 4995.00),
+    Place("Buenos Aires", "AR", 6912.28),
+    Place("Melbourne", "AU", 8149.70),
+    Place("Kishtwar", "IN", 6765.31),
+    Place("Lyon", "FR", 5285.99),
+]
+
+text = open("data.txt", "w")
+text.write("Location/Country/Distance" + "\n")
+for i in range(len(array)):
+    text.write(array[i].location + "/" + array[i].country + "/" + str(array[i].distance) + "\n")
+text.close()
+
+command = input("Enter 1 to export list to .csv ")
+
+if command == "1":
+    prompt = "run"
+    text = open("export_service.txt", "w")
+    text.write(prompt)
+    text.close()
+
+    time.sleep(1)
+    text = open("export_response.txt", "r")
+    print(text.read())
+    text.close()
+    text = open("export_response.txt", "w")
+    text.close()
